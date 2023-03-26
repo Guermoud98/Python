e5b3f65bf56cc946fbaff79532dfbdd69147d869
Gur <guermoud.@gmail.com>
2023-03-26 13:48:25 +0100

    Add a new function called pow_List

--- Exercice2.py
+++ Exercice2.py
@@ -1,6 +1,6 @@
 import math
 # Finding the unique elements in a list
-def unique(list):
+def unique_element(list):
     li = []
     for i in list:
         if i not in li:
@@ -12,9 +12,9 @@ while t != 0:
     num = int(input("give me an integer: "))
     list.append(num)
     t = t - 1
-u = unique(list)
+u = unique_element(list)
 print(u)
-# returning a new list containing the elements in common of two lists.
+# returning a new list containing the elements common to two lists.
 def unique (list1, list2):
     li = set(list1) & set(list2)
     return li
@@ -23,19 +23,19 @@ list2 = [1,3,10,12,15]
 u = unique(list1,list2)
 print(u)
 
-# finding all the strings with a length > 4
-def lengthSupFour(list):
+# finding all strings with a length > 4
+def length_sup_four(list):
     li = []
     for i in list:
         if len(i) > 4:
             li.append(i)
     return li
 fruits = ["apple", "banana", "fig"]
-fruits_list = lengthSupFour(fruits)
+fruits_list = length_sup_four(fruits)
 print(fruits_list)
 
 # finding the integers > 10 
-def supTen(list):
+def sup_ten(list):
     li = []
     for i in list:
         if i > 10:
@@ -47,17 +47,17 @@ while t != 0:
     num = int(input("give me an integer: "))
     list.append(num)
     t = t - 1
-sup_ten = supTen(list)
-print(sup_ten)
+sup = sup_ten(list)
+print(sup)
 
-# Converting strings to Uppercase
-def stringUpperCase(list):
+# Converting strings to uppercase
+def string_upper_case(list):
     li = []
     for i in list:
         li.append(i.upper())
     return li
 list = ["hello","world"]
-li_upper = stringUpperCase(list)
+li_upper = string_upper_case(list)
 print(li_upper)
 
 # sum of elements in a list
@@ -69,13 +69,23 @@ def somme(list):
 list = [1,2,3,4]
 print("The sum of all the elements in the given list is ", somme(list))
 
-# rteurning a list of the squares of integers.
-def powList(list):
+# returning a list of the square of integers.
+def square_list(list):
+    racine_liste = []
+    for i in list:
+        racine_liste.append(math.sqrt(i))
+    return racine_liste
+list = [1,2,4]
+li_square = square_list(list)
+print(li_square)
+
+# returning a list of the powers of integers.
+
+def pow_list(list):
     carres_liste = []
     for i in list:
-        carres_liste.append(math.sqrt(i))
+        carres_liste.append(i ** i)
     return carres_liste
-list = [1,2,4]
-li_pow = powList(list)
+list = [1,2]
+li_pow = pow_list(list)
 print(li_pow)
-
